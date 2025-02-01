ca74699059671045560e16bf825833e14cd0e966
Eunum56 <msiaffu123@gmail.com>
2025-02-01 15:03:15 +0530

    Implementing chainlink VRF

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -2,22 +2,40 @@
 
 pragma solidity ^0.8.24;
 
-contract Raffle {
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
+contract Raffle is VRFConsumerBaseV2Plus {
     /* Errors */
     error Raffle__NotEnoughEthSent();
 
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
     uint256 private immutable i_entranceFee;
-    address payable[] private s_players;
     uint256 private immutable i_interval;
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    address payable[] private s_players;
     uint256 private s_lastTimeStamp;
 
     /* Events */
     event RaffleEntered(address player);
 
-    constructor(uint256 entranceFee, uint256 interval, uint256 lastTimeStamp) {
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 keyHash,
+        uint256 subscriptionId,
+        uint32 casebackGasLimit
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_entranceFee = entranceFee;
         i_interval = interval;
-        s_lastTimeStamp = lastTimeStamp;
+        s_lastTimeStamp = block.timestamp;
+        i_keyHash = keyHash;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = casebackGasLimit;
     }
 
     function enterRaffle() public payable {
@@ -29,12 +47,40 @@ contract Raffle {
         s_players.push(payable(msg.sender));
         emit RaffleEntered(msg.sender);
     }
+    // 1 Get a random number.
+    // 2 Pick a winner.
+    // 3 Transfer the prize to the winner.
+    // 4 Reset the players array.
+    // 5 Reset the lottery timestamp.
 
     function pickWinner() public {
+        // check to see if enough time has passed
         if ((block.timestamp - s_lastTimeStamp > i_interval)) {
             revert();
         }
+
+        // Getting a random number from chainlink VRF
+        bool enableNativePayment = true;
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: i_keyHash,
+                subId: i_subscriptionId,
+                requestConfirmations: REQUEST_CONFIRMATIONS,
+                callbackGasLimit: i_callbackGasLimit,
+                numWords: NUM_WORDS,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({
+                        nativePayment: enableNativePayment
+                    })
+                )
+            })
+        );
     }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] calldata randomWords
+    ) internal override {}
 
     // Getters
     function getEntranceFee() external view returns (uint256) {
