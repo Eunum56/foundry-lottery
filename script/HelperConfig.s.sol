bb354bfb25bb19addc30da07bff23477437f871c
Eunum56 <msiaffu123@gmail.com>
2025-02-03 19:23:58 +0530

    Add localNetworkConfig

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,9 +2,14 @@
 pragma solidity ^0.8.24;
 
 import {Script} from "forge-std/Script.sol";
-import {VRFCoodinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoodinatorV2_5Mock.sol";
+import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 
 abstract contract ChainConstants {
+    /* VRF MOCKS VALUES */
+    uint96 public MOCK_BASE_FEE = 0.25 ether;
+    uint96 public MOCK_GAS_PRICE_LINK = 1e9;
+    int256 public MOCK_WEI_PER_UINT_LINK = 4e15;
+
     uint256 public constant SPEOLIA_ETH_CHAIN_ID = 11155111;
     uint256 public constant LOCAL_CHAIN_ID = 31337;
 }
@@ -30,7 +35,7 @@ contract HelperConfig is ChainConstants, Script {
 
     function getConfigByChainId(
         uint256 chainId
-    ) public view returns (NetworkConfig memory) {
+    ) public returns (NetworkConfig memory) {
         if (networkConfigs[chainId].vrfCoordinator != address(0)) {
             return networkConfigs[chainId];
         } else if (chainId == LOCAL_CHAIN_ID) {
@@ -56,5 +61,25 @@ contract HelperConfig is ChainConstants, Script {
         if (localNetworkConfig.vrfCoordinator != address(0)) {
             return localNetworkConfig;
         }
+
+        // Deploy mock
+        vm.startBroadcast();
+        VRFCoordinatorV2_5Mock vrfCoordinator = new VRFCoordinatorV2_5Mock(
+            MOCK_BASE_FEE,
+            MOCK_GAS_PRICE_LINK,
+            MOCK_WEI_PER_UINT_LINK
+        );
+        vm.stopBroadcast();
+
+        localNetworkConfig = NetworkConfig({
+            entranceFee: 0.01 ether,
+            interval: 30,
+            vrfCoordinator: address(vrfCoordinator),
+            // keyHash does'not matter IDK
+            keyHash: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+            callBackGasLimit: 500000,
+            subscriptionId: 0 // might need to change subscriptionId
+        });
+        return localNetworkConfig;
     }
 }
