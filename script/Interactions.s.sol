7350a127cc854cbf5f8c19036e892ca8141e02a3
Eunum56 <msiaffu123@gmail.com>
2025-02-04 21:53:25 +0530

    Add AddConsumer contract

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -5,6 +5,7 @@ import {Script, console} from "forge-std/Script.sol";
 import {HelperConfig, ChainConstants} from "./HelperConfig.s.sol";
 import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 import {LinkToken} from "../test/mocks/LinkToken.sol";
+import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
 
 contract CreateSubscription is Script {
     function createSubscriptionIdUsingConfig()
@@ -24,8 +25,11 @@ contract CreateSubscription is Script {
             "Creating subscription using vrfCoordinator: ",
             vrfCoordinator
         );
+        console.log("Creating on chainId: ", block.chainid);
+        vm.startBroadcast();
         uint256 subId = VRFCoordinatorV2_5Mock(vrfCoordinator)
             .createSubscription();
+        vm.stopBroadcast();
         console.log("Subscription Id: ", subId);
         return (subId, vrfCoordinator);
     }
@@ -76,5 +80,39 @@ contract FundSubscription is Script, ChainConstants {
 
     function run() external {
         fundSubscriptionUsingConfig();
+    }
+}
+
+contract AddConsumer is Script {
+    function addConsumerUsingConfig(address mostRecentDeploy) public {
+        HelperConfig helperConfig = new HelperConfig();
+        uint256 subId = helperConfig.getConfig().subscriptionId;
+        address vrfCoordinator = helperConfig.getConfig().vrfCoordinator;
+        addConsumer(mostRecentDeploy, vrfCoordinator, subId);
+    }
+
+    function addConsumer(
+        address mostRecentDeploy,
+        address vrfCoordinator,
+        uint256 subId
+    ) public {
+        console.log("Adding consumer to:", mostRecentDeploy);
+        console.log("Using vrfCoordinator:", vrfCoordinator);
+        console.log("Using subscriptionId:", subId);
+
+        vm.startBroadcast();
+        VRFCoordinatorV2_5Mock(vrfCoordinator).addConsumer(
+            subId,
+            mostRecentDeploy
+        );
+        vm.stopBroadcast();
+    }
+
+    function run() external {
+        address mostRecentDeploy = DevOpsTools.get_most_recent_deployment(
+            "Raffle",
+            block.chainid
+        );
+        addConsumerUsingConfig(mostRecentDeploy);
     }
 }
